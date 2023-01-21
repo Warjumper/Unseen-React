844a098633eec2ce24f1d1afd73af4ca8010b3b5
unknown <jessehorner11@gmail.com>
2023-01-20 17:07:06 -0700

    initial commit

---
+++ src/components/Hero/Hero.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
+import Button from '../../styles/GlobalComponents/Button';
+import { LeftSection } from './HeroStyles';
+
+const Hero = () => (
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome To <br />
+        My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        At Unseen Riches we believe there is a better way to connect you to some of the great events going on every single day. Our focus is to simplify the way you discover entertainment in your city.
+      </SectionText>
+      <Button onClick={() => window.location = 'https://google.com'} >Explore</Button>
+    </LeftSection>
+  </Section>
+);
+
+export default Hero;
---
+++ src/components/Projects/Projects.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
+import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
+import { projects } from '../../constants/constants';
+
+const Projects = () => (
+  <Section nopadding id="projects">
+    <SectionDivider />
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+      {projects.map(({ id, image, title, description, tages, source, visit }) =>(
+        <BlogCard key={id}>
+          <Img src={image} />
+          <TitleContent>
+            <HeaderThree title>{title}</HeaderThree>
+            <Hr />
+          </TitleContent>
+          <CardInfo style={{marginBottom: '20px'}} >{description}</CardInfo>
+          <div>
+            <TitleContent style={{marginBottom: '20px'}} >Stack</TitleContent>
+{/* TAG UNDEFINED */}
+            {/* <TagList>
+              {tags.map((tag, i) => (
+                  <Tag key={i}>{tag}</Tag>
+              ))}
+            </TagList> */}
+          </div>
+
+{/* CODE LINKS */}
+          {/* <UtilityList>
+            <ExternalLinks href={visit}>Code</ExternalLinks>
+            <ExternalLinks href={source}>Source</ExternalLinks>
+          </UtilityList> */}
+        </BlogCard>
+      ))}
+    </GridContainer>
+  </Section>
+);
+
+export default Projects;
