7eec9bc4d61ddfeec5d550a099158b417d9ce9ab
unknown <jessehorner11@gmail.com>
2023-02-01 21:16:07 -0700

    comment out cards for now

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
-import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
+import { Section, SectionDivider, SectionTitle, SectionText, } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
 
 const Projects = () => (
   <Section nopadding id="projects">
     <SectionDivider />
     <SectionTitle main>Nightlife</SectionTitle>
+    <SectionText>
+      Stay tuned for a list of your upcoming Calgary Nightlife 2023 events!
+    </SectionText>
     <GridContainer>
       {projects.map(({ id, image, title, description, tages, source, visit }) =>(
         <BlogCard key={id}>
--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,84 +1,84 @@
 // CARD EVENT DATA
 export const projects = [
-  {
-    title: "National on 10th",
-    description: "National is a true destination for fun with friends, best-in-class craft beer, handcrafted cocktails and a thoughtfully curated food menu. The lower level of National 10 features a trendy 8-lane bowling alley complete with arcade games, music, drinks and a full menu.",
-    image: "/images/website-national.png",
-    tags: ["", "", "", ""],
-    // TICKETS
-    source: "",
-    // WEBPAGE LINK URL
-    visit: "https://www.ntnl.ca/10th-avenue",
-    id: 0,
-  },
-  {
-    title: "Craft",
-    description: "Craft is a premium casual restaurant and bar with a passion for fresh local food and great craft beer. Launched in Calgary in 2011, the CRAFT movement has been spreading across Canada ever since.",
-    image: "/images/craft-nobg.png",
-    tags: ["", ""],
-    // TICKETS
-    source: "",
-    // WEBPAGE LINK URL
-    visit: "https://www.craftbeermarket.ca/",
-    id: 1,
-  },
-  {
-    title: "Greta Bar",
-    description: "It's all fun and games at your favourite arcade bar. Join us at GRETA in YYC or YEG for good times, great food, and the best games.",
-    image: "/images/website-greta.png",
-    tags: ["", ""],
-    // TICKETS
-    source: "",
-    // WEBPAGE LINK URL
-    visit: "https://www.gretabar.com/",
-    id: 2,
-  },
-  {
-    title: "Wildhorse Saloon",
-    description: "The Wildhorse Saloon is your destination for the greatest Stampede Party in Calgary. Located in the heart of downtown, they have daily concerts, western barbecue, Double Zero pizza, a non-stop dance floor, an outdoor midway, and 20+ bars serving drinks that will knock your boots off!",
-    image: "/images/stampede-wildhorse.png",
-    tags: ["", "", ""],
-    // TICKETS
-    source: "",
-    // WEBPAGE LINK URL
-    visit: "https://www.wildhorsesaloon.ca/",
-    id: 3,
-  },
-  {
-    title: "National Saloon",
-    description: "Located behind National 10th in the heart of Calgary’s beltline is the newest tent experience. Live music, great food and the good times you know and love from National. Your one-stop-shop this summer for all your partying needs.",
-    image: "/images/stampede-ntnlsaloon.png",
-    tags: ["", "", ""],
-    // TICKETS
-    source: "",
-    // WEBPAGE LINK URL
-    visit: "https://www.ntnlsaloon.ca/",
-    id: 4,
-  },
-  {
-    title: "Cowboys",
-    description:
-      "The World Famous Cowboys Dance Hall. Calgary's biggest nightclub featuring country and top 40 music, weekly events and live music. The most fun you can have with your boots on!",
-    image: "/images/stampede-cowboys.png",
-    tags: ["", "", ""],
-    // TICKETS
-    source: "",
-    // WEBPAGE LINK URL
-    visit: "https://www.cowboysmusicfestival.com/",
-    id: 5,
-  },
-  {
-    title: "Badlands Festival",
-    description:
-      "The Badlands Festival is a series of electronic and hip-hop shows that takes place downtown during the famous Calgary Stampede.",
-    image: "/images/stampede-badlands.png",
-    tags: ["", "", ""],
-    // TICKETS
-    source: "",
-    // WEBPAGE LINK URL
-    visit: "https://www.badlandsmusicfest.com/",
-    id: 6,
-  },
+  // {
+  //   title: "National on 10th",
+  //   description: "National is a true destination for fun with friends, best-in-class craft beer, handcrafted cocktails and a thoughtfully curated food menu. The lower level of National 10 features a trendy 8-lane bowling alley complete with arcade games, music, drinks and a full menu.",
+  //   image: "/images/website-national.png",
+  //   tags: ["", "", "", ""],
+  //   // TICKETS
+  //   source: "",
+  //   // WEBPAGE LINK URL
+  //   visit: "https://www.ntnl.ca/10th-avenue",
+  //   id: 0,
+  // },
+  // {
+  //   title: "Craft",
+  //   description: "Craft is a premium casual restaurant and bar with a passion for fresh local food and great craft beer. Launched in Calgary in 2011, the CRAFT movement has been spreading across Canada ever since.",
+  //   image: "/images/craft-nobg.png",
+  //   tags: ["", ""],
+  //   // TICKETS
+  //   source: "",
+  //   // WEBPAGE LINK URL
+  //   visit: "https://www.craftbeermarket.ca/",
+  //   id: 1,
+  // },
+  // {
+  //   title: "Greta Bar",
+  //   description: "It's all fun and games at your favourite arcade bar. Join us at GRETA in YYC or YEG for good times, great food, and the best games.",
+  //   image: "/images/website-greta.png",
+  //   tags: ["", ""],
+  //   // TICKETS
+  //   source: "",
+  //   // WEBPAGE LINK URL
+  //   visit: "https://www.gretabar.com/",
+  //   id: 2,
+  // },
+  // {
+  //   title: "Wildhorse Saloon",
+  //   description: "The Wildhorse Saloon is your destination for the greatest Stampede Party in Calgary. Located in the heart of downtown, they have daily concerts, western barbecue, Double Zero pizza, a non-stop dance floor, an outdoor midway, and 20+ bars serving drinks that will knock your boots off!",
+  //   image: "/images/stampede-wildhorse.png",
+  //   tags: ["", "", ""],
+  //   // TICKETS
+  //   source: "",
+  //   // WEBPAGE LINK URL
+  //   visit: "https://www.wildhorsesaloon.ca/",
+  //   id: 3,
+  // },
+  // {
+  //   title: "National Saloon",
+  //   description: "Located behind National 10th in the heart of Calgary’s beltline is the newest tent experience. Live music, great food and the good times you know and love from National. Your one-stop-shop this summer for all your partying needs.",
+  //   image: "/images/stampede-ntnlsaloon.png",
+  //   tags: ["", "", ""],
+  //   // TICKETS
+  //   source: "",
+  //   // WEBPAGE LINK URL
+  //   visit: "https://www.ntnlsaloon.ca/",
+  //   id: 4,
+  // },
+  // {
+  //   title: "Cowboys",
+  //   description:
+  //     "The World Famous Cowboys Dance Hall. Calgary's biggest nightclub featuring country and top 40 music, weekly events and live music. The most fun you can have with your boots on!",
+  //   image: "/images/stampede-cowboys.png",
+  //   tags: ["", "", ""],
+  //   // TICKETS
+  //   source: "",
+  //   // WEBPAGE LINK URL
+  //   visit: "https://www.cowboysmusicfestival.com/",
+  //   id: 5,
+  // },
+  // {
+  //   title: "Badlands Festival",
+  //   description:
+  //     "The Badlands Festival is a series of electronic and hip-hop shows that takes place downtown during the famous Calgary Stampede.",
+  //   image: "/images/stampede-badlands.png",
+  //   tags: ["", "", ""],
+  //   // TICKETS
+  //   source: "",
+  //   // WEBPAGE LINK URL
+  //   visit: "https://www.badlandsmusicfest.com/",
+  //   id: 6,
+  // },
   // {
   //   title: 'Unichat',
   //   description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
