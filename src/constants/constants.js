2749c406e3595e358bca5df5e2a01075b842bd27
unknown <jessehorner11@gmail.com>
2023-02-21 04:08:53 -0700

    new images and card data for nightlife

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,111 +1,173 @@
-// CARD EVENT DATA
+// NIGHTLIFE CARD DATA
 export const projects = [
-  // {
-  //   title: "National on 10th",
-  //   description: "National is a true destination for fun with friends, best-in-class craft beer, handcrafted cocktails and a thoughtfully curated food menu. The lower level of National 10 features a trendy 8-lane bowling alley complete with arcade games, music, drinks and a full menu.",
-  //   image: "/images/website-national.png",
-  //   tags: ["", "", "", ""],
-  //   // TICKETS
-  //   source: "",
-  //   // WEBPAGE LINK URL
-  //   visit: "https://www.ntnl.ca/10th-avenue",
-  //   id: 0,
-  // },
-  // {
-  //   title: "Craft",
-  //   description: "Craft is a premium casual restaurant and bar with a passion for fresh local food and great craft beer. Launched in Calgary in 2011, the CRAFT movement has been spreading across Canada ever since.",
-  //   image: "/images/craft-nobg.png",
-  //   tags: ["", ""],
-  //   // TICKETS
-  //   source: "",
-  //   // WEBPAGE LINK URL
-  //   visit: "https://www.craftbeermarket.ca/",
-  //   id: 1,
-  // },
-  // {
-  //   title: "Greta Bar",
-  //   description: "It's all fun and games at your favourite arcade bar. Join us at GRETA in YYC or YEG for good times, great food, and the best games.",
-  //   image: "/images/website-greta.png",
-  //   tags: ["", ""],
-  //   // TICKETS
-  //   source: "",
-  //   // WEBPAGE LINK URL
-  //   visit: "https://www.gretabar.com/",
-  //   id: 2,
-  // },
-  // {
-  //   title: "Wildhorse Saloon",
-  //   description: "The Wildhorse Saloon is your destination for the greatest Stampede Party in Calgary. Located in the heart of downtown, they have daily concerts, western barbecue, Double Zero pizza, a non-stop dance floor, an outdoor midway, and 20+ bars serving drinks that will knock your boots off!",
-  //   image: "/images/stampede-wildhorse.png",
-  //   tags: ["", "", ""],
-  //   // TICKETS
-  //   source: "",
-  //   // WEBPAGE LINK URL
-  //   visit: "https://www.wildhorsesaloon.ca/",
-  //   id: 3,
-  // },
-  // {
-  //   title: "National Saloon",
-  //   description: "Located behind National 10th in the heart of Calgary’s beltline is the newest tent experience. Live music, great food and the good times you know and love from National. Your one-stop-shop this summer for all your partying needs.",
-  //   image: "/images/stampede-ntnlsaloon.png",
-  //   tags: ["", "", ""],
-  //   // TICKETS
-  //   source: "",
-  //   // WEBPAGE LINK URL
-  //   visit: "https://www.ntnlsaloon.ca/",
-  //   id: 4,
-  // },
-  // {
-  //   title: "Cowboys",
-  //   description:
-  //     "The World Famous Cowboys Dance Hall. Calgary's biggest nightclub featuring country and top 40 music, weekly events and live music. The most fun you can have with your boots on!",
-  //   image: "/images/stampede-cowboys.png",
-  //   tags: ["", "", ""],
-  //   // TICKETS
-  //   source: "",
-  //   // WEBPAGE LINK URL
-  //   visit: "https://www.cowboysmusicfestival.com/",
-  //   id: 5,
-  // },
-  // {
-  //   title: "Badlands Festival",
-  //   description:
-  //     "The Badlands Festival is a series of electronic and hip-hop shows that takes place downtown during the famous Calgary Stampede.",
-  //   image: "/images/stampede-badlands.png",
-  //   tags: ["", "", ""],
-  //   // TICKETS
-  //   source: "",
-  //   // WEBPAGE LINK URL
-  //   visit: "https://www.badlandsmusicfest.com/",
-  //   id: 6,
-  // },
-  // {
-  //   title: 'Unichat',
-  //   description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-  //   image: '/images/',
-  //   tags: ['React', '', ''],
-  //   source: 'https://google.com',
-  //   visit: 'https://google.com',
-  //   id: 7,
-  // },
-  // {
-  //   title: 'Unichat',
-  //   description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-  //   image: '/images/',
-  //   tags: ['React', '', ''],
-  //   source: 'https://google.com',
-  //   visit: 'https://google.com',
-  //   id: 8,
-  // },
-  // {
-  //   title: 'Unichat',
-  //   description: "This is a code repository for the corresponding video tutorial. In this video, we will create a full Realtime Chat Application",
-  //   image: '/images/',
-  //   tags: ['React', '', ''],
-  //   source: 'https://google.com',
-  //   visit: 'https://google.com',
-  //   id: 9,
-  // },
+  {
+    title: "National on 10th",
+    description: "National is a true destination for fun with friends, best-in-class craft beer, handcrafted cocktails and a thoughtfully curated food menu. The lower level of National 10 features a trendy 8-lane bowling alley complete with arcade games, music, drinks and a full menu.",
+    image: "/images/website-national.png",
+    tags: ["", "", "", ""],
+    // TICKETS/GLIST
+    source: "",
+    // WEBPAGE LINK URL
+    visit: "https://www.ntnl.ca/10th-avenue",
+    id: 0,
+  },
+  {
+    title: "Craft",
+    description: "Craft is a premium casual restaurant and bar with a passion for fresh local food and great craft beer. Launched in Calgary in 2011, the CRAFT movement has been spreading across Canada ever since.",
+    image: "/images/website-craft.png",
+    tags: ["", ""],
+    // TICKETS
+    source: "",
+    // WEBPAGE LINK URL
+    visit: "https://www.craftbeermarket.ca/",
+    id: 1,
+  },
+  {
+    title: "Greta Bar",
+    description: "It's all fun and games at your favourite arcade bar. Join us at GRETA in YYC or YEG for good times, great food, and the best games.",
+    image: "/images/website-greta.png",
+    tags: ["", ""],
+    // TICKETS
+    source: "",
+    // WEBPAGE LINK URL
+    visit: "https://www.gretabar.com/",
+    id: 2,
+  },
+  {
+    title: "Honky-tonk",
+    description: "A new country saloon-style bar, just opened on 10th Avenue, making it an ideal location to dance the night away.",
+    image: "/images/website-honkeytonk.png",
+    tags: ["", "", ""],
+    // TICKETS
+    source: "",
+    // WEBPAGE LINK URL
+    visit: "",
+    id: 3,
+  },
+  {
+    title: "Whiskey Rose",
+    description: "Whiskey Rose Saloon is a bar to come together, listen to all kinds of music, and knock back a bourbon (or six).",
+    image: "/images/website-whiskeyrose.png",
+    tags: ["", "", ""],
+    // TICKETS
+    source: "",
+    // WEBPAGE LINK URL
+    visit: "https://whiskeyrosesaloon.com/",
+    id: 4,
+  },
+  {
+    title: "Cowboys",
+    description:
+      "The World Famous Cowboys Dance Hall. Calgary's biggest nightclub featuring country and top 40 music, weekly events and live music. The most fun you can have with your boots on!",
+    image: "/images/website-cowboys.png",
+    tags: ["", "", ""],
+    // TICKETS
+    source: "",
+    // WEBPAGE LINK URL
+    visit: "https://www.cowboysmusicfestival.com/",
+    id: 5,
+  },
+  {
+    title: "Trolley5",
+    description:
+      "Industrial-style space with outdoor seating serving house-brewed beers, burgers, pizza & BBQ.",
+    image: "/images/website-trolley5.png",
+    tags: ["", "", ""],
+    // TICKETS
+    source: "",
+    // WEBPAGE LINK URL
+    visit: "https://trolley5.com/",
+    id: 6,
+  },
+  {
+    title: "One Night Stan's",
+    description: "One Night Stan's is the kind of place where you can feel comfortable in a three piece suit, yoga pants, or anything in between.",
+    image: "/images/website-onenightstans.png",
+    tags: ['React', '', ''],
+    // TICKETS/GLIST
+    source: '',
+    // WEBPAGE LINK URL
+    visit: 'https://www.onenightstans.ca/',
+    id: 7,
+  },
+  {
+    title: 'Ship and Anchor',
+    description: "Low-key watering hole offering casual bar fare, sports on TV & occasional live music, plus a patio.",
+    image: '/images/website-shipandanchor.png',
+    tags: ['React', '', ''],
+    // TICKETS/GLIST
+    source: '',
+    // WEBPAGE LINK URL
+    visit: 'https://shipandanchor.com/',
+    id: 8,
+  },
+  {
+    title: 'The Palace',
+    description: "This multi-function venue will be home to everything from concerts, musical performances, seminars, symposiums & corporate events. All with our state-of-the art cinema sound system!",
+    image: '/images/website-palace.png',
+    tags: ['React', '', ''],
+    // TICKETS/GLIST
+    source: '',
+    // WEBPAGE LINK URL
+    visit: 'https://www.thepalacetheatre.ca/',
+    id: 9,
+  },
+  {
+    title: "Hudson's",
+    description: "Canada's pub is your go-to for great deals and happy hour specials!",
+    image: '/images/website-hudsons.png',
+    tags: ['React', '', ''],
+    // TICKETS/GLIST
+    source: '',
+    // WEBPAGE LINK URL
+    visit: 'https://hudsonscanadaspub.com/',
+    id: 10,
+  },
+  {
+    title: 'The Back Alley',
+    description: "The Back Alley Nightclub is over 12000 feet and harbours 4 themed bars which include Bacardi, Jack, Red Bull and Jamesons.",
+    image: '/images/website-backalley.png',
+    tags: ['React', '', ''],
+    // TICKETS/GLIST
+    source: '',
+    // WEBPAGE LINK URL
+    visit: 'https://backalleycalgary.com/',
+    id: 11,
+  },
+  {
+    title: 'Commonwealth',
+    description: "Rustic-chic nightclub with DJ shows & dance floors on 2 floors, plus a menu of upscale bar bites.",
+    image: '/images/website-commonwealth.png',
+    tags: ['React', '', ''],
+    // TICKETS/GLIST
+    source: '',
+    // WEBPAGE LINK URL
+    visit: 'https://www.commonwealthbar.ca/',
+    id: 12,
+  },
+  {
+    title: "Ranchman's",
+    description: "Western bar with a dance floor, stage & mechanical bull, plus a pub menu of burgers, ribs & wings.",
+    image: '/images/website-ranchmans.png',
+    tags: ['React', '', ''],
+    // TICKETS/GLIST
+    source: '',
+    // WEBPAGE LINK URL
+    visit: 'https://www.ranchmans.ca/',
+    id: 13,
+  },
+  {
+    title: 'The Banquet',
+    description: "Premium Bar, a bowling alley, patio, games & more in Calgary's University district!",
+    image: '/images/website-banquet.png',
+    tags: ['React', '', ''],
+    // TICKETS/GLIST
+    source: '',
+    // WEBPAGE LINK URL
+    visit: 'https://thebanquetbar.com/',
+    id: 14,
+  },
+
 ];
 
 // CAROUSEL TIMELINE
